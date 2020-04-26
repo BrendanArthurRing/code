1afb8db228f3706f80dd6212cf09ce17306a0910
BrendanArthurRing <brendanarthurring@gmail.com>
2020-04-26 09:48:22 +0900

    initial commit

--- range_bit_count.py
+++
@@ -1,8 +0,0 @@
-def rangeBitCount(a, b):
-    return ''.join([f'{i:b}' for i in range(a, b + 1)]).count('1')
-
-# The top voted solution
-
-
-def rangeBitCount(a, b):
-    return sum(bin(x).count('1') for x in range(a, b+1))
